229693596daa432bd9aacd7f05215828970dbd64
HanxiangLiu <hanxiangliu529@gmail.com>
2023-11-08 22:41:43 -0800

    Update ABC Algo

--- main.py
+++ main.py
@@ -7,25 +7,28 @@ from plotting import initialize_plots, update_plot
 from algorithms.pso_algorithm import PSOAlgorithm
 from algorithms.ga_algorithm import GAAlgorithm
 from algorithms.aco_algorithm import ACOAlgorithm
+from algorithms.abc_algorithm import ABCAlgorithm 
 
 # Parameters for algorithm initialization (these should be defined according to your problem and algorithm requirements)
 num_particles = 50  # This is an example, adjust accordingly
 search_space = [-512, 512]  # Example search space
-num_iterations = 100  # Number of iterations for the animation
+num_iterations = 50  # Number of iterations for the animation
 pso_params = {'num_particles': num_particles, 'search_space': search_space, 'w': 0.5, 'c1': 0.1, 'c2': 0.1}
 ga_params = {'population_size': num_particles, 'search_space': search_space}
 aco_params = {'num_ants': num_particles, 'search_space': search_space, 'alpha': 1.0, 'decay': 0.1}
+abc_params = {'num_bees': num_particles, 'search_space': search_space}
 
 # Initialize algorithms with the necessary parameters
 pso = PSOAlgorithm(**pso_params)
 ga = GAAlgorithm(**ga_params)
 aco = ACOAlgorithm(**aco_params)
+abc = ABCAlgorithm(**abc_params)
 
 # Initialize plots for each algorithm
 fig, axes = initialize_plots()
 
 # Define labels for the legend of each subplot
-algorithm_names = ['PSO', 'GA', 'ACO']
+algorithm_names = ['PSO', 'GA', 'ACO', 'ABC'] 
 
 # Initialize empty scatter plots with labels for the legend
 scatter_plots = []
@@ -34,13 +37,14 @@ for ax, name in zip(axes, algorithm_names):
     scatter_plots.append(scatter)
 
 # Create legends based on the initial (empty) scatter plots
-for ax in axes:
+for ax in axes[:4]:  # Only for the first four plots
     ax.legend(loc='upper right')
 
 # Lists to keep track of scatter plot artists for each algorithm
 scatter_plots_pso = []
 scatter_plots_ga = []
 scatter_plots_aco = []
+scatter_plots_abc = []  
 
 # Text objects to display the iteration number
 iteration_texts = [ax.text2D(0.05, 0.95, '', transform=ax.transAxes) for ax in axes]
@@ -51,24 +55,28 @@ def update(frame):
     pso.update()
     ga.update()
     aco.update()
+    abc.update()
 
     # Clear the previous scatter plots for each algorithm
-    for scat in scatter_plots_pso + scatter_plots_ga + scatter_plots_aco:
+    for scat in scatter_plots_pso + scatter_plots_ga + scatter_plots_aco + scatter_plots_abc:
         scat.remove()
 
     scatter_plots_pso.clear()
     scatter_plots_ga.clear()
     scatter_plots_aco.clear()
+    scatter_plots_abc.clear()
 
     # Update plots for each algorithm on their respective axis
     pso_data = pso.get_positions()
     ga_data = ga.get_positions()
     aco_data = aco.get_positions()
+    abc_data = abc.get_positions()
 
     # Plot and store the scatter plot artists
     scatter_plots_pso.append(update_plot(axes[0], pso_data, 'red', 'PSO'))
     scatter_plots_ga.append(update_plot(axes[1], ga_data, 'blue', 'GA'))
     scatter_plots_aco.append(update_plot(axes[2], aco_data, 'purple', 'ACO'))
+    scatter_plots_abc.append(update_plot(axes[3], abc_data, 'black', 'ABC'))
 
     # Update iteration number and algorithm name
     for iter_text in iteration_texts:
--- plotting.py
+++ plotting.py
@@ -1,22 +1,27 @@
 # plotting.py
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 import numpy as np
 
 from functions.objective_function import schwefel_function
 
 def initialize_plots():
-    fig = plt.figure(figsize=(18, 6))
+    fig = plt.figure(figsize=(18, 12))  # Adjusted the figure size for 2x3 layout
 
-    # Create three subplots for PSO, GA, and ACO
-    ax1 = fig.add_subplot(131, projection='3d')
-    ax2 = fig.add_subplot(132, projection='3d')
-    ax3 = fig.add_subplot(133, projection='3d')
-    axes = [ax1, ax2, ax3]
+    # Create six subplots for algorithms and two placeholders
+    # First row: PSO, GA, ACO
+    ax1 = fig.add_subplot(231, projection='3d')
+    ax2 = fig.add_subplot(232, projection='3d')
+    ax3 = fig.add_subplot(233, projection='3d')
+    # Second row: ABC, Placeholder 1, Placeholder 2
+    ax4 = fig.add_subplot(234, projection='3d')
+    ax5 = fig.add_subplot(235, projection='3d')  # Placeholder, no algorithm yet
+    ax6 = fig.add_subplot(236, projection='3d')  # Placeholder, no algorithm yet
 
-    # Plot the surface on each subplot
-    for ax in axes:
+    axes = [ax1, ax2, ax3, ax4, ax5, ax6]
+
+    # Plot the surface on each active subplot
+    for ax in axes[:-2]:  # Skipping the last two placeholders
         x = np.linspace(-512, 512, 400)
         y = np.linspace(-512, 512, 400)
         X, Y = np.meshgrid(x, y)
