22fa923a394c40112d1a2bf9d775a9528c4ec720
HanxiangLiu <hanxiangliu529@gmail.com>
2023-11-08 20:18:08 -0800

    Every algorithm has a figure

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from algorithm_interface import OptimizationAlgorithm
-from plotting import initialize_plot, update_plot
+from plotting import initialize_plots, update_plot
 from matplotlib.animation import FuncAnimation
 
 # Import the algorithm classes
@@ -23,14 +23,13 @@ pso = PSOAlgorithm(**pso_params)
 ga = GAAlgorithm(**ga_params)
 aco = ACOAlgorithm(**aco_params)
 
-# Initialize plot
-fig, ax = initialize_plot()
+# Initialize plots for each algorithm
+fig, axes = initialize_plots()
 
-# Set the view angle
-ax.view_init(elev=34, azim=134)
-
-# List to keep track of scatter plot artists for clearing them in each update
-scatter_plots = []
+# Lists to keep track of scatter plot artists for each algorithm
+scatter_plots_pso = []
+scatter_plots_ga = []
+scatter_plots_aco = []
 
 # Prepare the animation update function
 def update(frame):
@@ -39,20 +38,23 @@ def update(frame):
     ga.update()
     aco.update()
 
-    # Clear the previous scatter plots
-    for scat in scatter_plots:
+    # Clear the previous scatter plots for each algorithm
+    for scat in scatter_plots_pso + scatter_plots_ga + scatter_plots_aco:
         scat.remove()
-    scatter_plots.clear()
 
-    # Update plots for each algorithm
+    scatter_plots_pso.clear()
+    scatter_plots_ga.clear()
+    scatter_plots_aco.clear()
+
+    # Update plots for each algorithm on their respective axis
     pso_data = pso.get_positions()
     ga_data = ga.get_positions()
     aco_data = aco.get_positions()
 
     # Plot and store the scatter plot artists
-    scatter_plots.append(update_plot(ax, pso_data, 'red', 'PSO'))
-    scatter_plots.append(update_plot(ax, ga_data, 'blue', 'GA'))
-    scatter_plots.append(update_plot(ax, aco_data, 'purple', 'ACO'))
+    scatter_plots_pso.append(update_plot(axes[0], pso_data, 'red', 'PSO'))
+    scatter_plots_ga.append(update_plot(axes[1], ga_data, 'blue', 'GA'))
+    scatter_plots_aco.append(update_plot(axes[2], aco_data, 'purple', 'ACO'))
 
     # Update the figure
     fig.canvas.draw()
@@ -62,5 +64,7 @@ def update(frame):
 ani = FuncAnimation(fig, update, frames=range(num_iterations), blit=False, interval=100, repeat=False)
 
 # Show the plot with the legend
-plt.legend()
+for ax in axes:
+    ax.legend()
+
 plt.show()
--- plotting.py
+++ plotting.py
@@ -5,17 +5,26 @@ from matplotlib import cm
 from objective_function import schwefel_function
 import numpy as np
 
-def initialize_plot():
-    fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(111, projection='3d')
-    x = np.linspace(-512, 512, 400)
-    y = np.linspace(-512, 512, 400)
-    X, Y = np.meshgrid(x, y)
-    Z = schwefel_function(X, Y)
-    ax.plot_surface(X, Y, Z, cmap=cm.viridis, alpha=0.3)
-    return fig, ax
+def initialize_plots():
+    fig = plt.figure(figsize=(18, 6))
+
+    # Create three subplots for PSO, GA, and ACO
+    ax1 = fig.add_subplot(131, projection='3d')
+    ax2 = fig.add_subplot(132, projection='3d')
+    ax3 = fig.add_subplot(133, projection='3d')
+    axes = [ax1, ax2, ax3]
+
+    # Plot the surface on each subplot
+    for ax in axes:
+        x = np.linspace(-512, 512, 400)
+        y = np.linspace(-512, 512, 400)
+        X, Y = np.meshgrid(x, y)
+        Z = schwefel_function(X, Y)
+        ax.plot_surface(X, Y, Z, cmap=cm.viridis, alpha=0.3)
+        ax.view_init(elev=34, azim=134)
+
+    return fig, axes
 
 def update_plot(ax, algorithm_data, color, label):
     X, Y, Z = algorithm_data
-    scat = ax.scatter(X, Y, Z, color=color, label=label)
-    return scat
+    return ax.scatter(X, Y, Z, color=color, label=label)
